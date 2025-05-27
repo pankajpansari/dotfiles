6780fe01e3d9716d0212fa0082f22088adc2598c
pankajpansari <pankaj.pansari1@gmail.com>
2025-05-27 04:43:00 +0000

    Added config for tmux. Use Ctrl+A binding

--- setup.sh
+++ setup.sh
@@ -25,6 +25,7 @@ echo -e "Adding symlinks"
 ln -sf dotfiles/vimrc ~/.vimrc
 ln -sf dotfiles/zshrc ~/.zshrc
 ln -sf dotfiles/gitconfig ~/.gitconfig
+ln -sf dotfiles/tmux.conf ~/.tmux.conf
 
 echo -e "Setup complete"
 
